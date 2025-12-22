9186bd31387598cc8dcdd19d6cd91ba4931835e3
Sahil Pate <sahilpate7@gmail.com>
2025-12-22 12:47:57 +0530

    feat: add API to fetch user connections
    - Added route to fetch accepted connection requests with user details.
    - Integrated `userAuth` middleware for authentication.
    - Populated user details in connection requests response.

--- src/routes/user.js
+++ src/routes/user.js
@@ -23,4 +23,27 @@ userRouter.get('/user/requests/received', userAuth, async (req,res)=>{
     }
 })
 
+userRouter.get('/user/connections', userAuth, async (req,res)=>{
+    try{
+        const loggedInUser = req.user;
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or : [
+                { toUserId : loggedInUser._id, status: "accepted"},
+                { fromUserId : loggedInUser._id, status: "accepted"}
+            ]
+        }).populate("fromUserId", ["firstName","lastName", "photoUrl" , "gender", "age"]);
+
+        const data = connectionRequest.map((row)=> row.fromUserId)
+
+        res.json({
+            message: "Connections fetched successfully",
+            data
+        })
+
+    } catch (err){
+        res.status(400).send("Error " + err.message);
+    }
+})
+
 module.exports = userRouter;
